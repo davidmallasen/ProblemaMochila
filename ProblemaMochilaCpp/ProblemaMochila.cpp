9ecc6283712a95108e4298b3f89466910e43e0af
David Mallasén <dmallase@ucm.es>
2017-04-10 15:08:52 +0200

    Hecho algoritmo de progDin, añadidos comentarios

--- ProblemaMochilaCpp/ProblemaMochila.cpp
+++ ProblemaMochilaCpp/ProblemaMochila.cpp
@@ -3,8 +3,15 @@
 #include <algorithm>
 #include <chrono>
 
-struct Objeto {
+//Estructuras auxiliares -------------------------------------------------------
+
+struct ObjetoReal {
     double peso;
+    double valor;
+};
+
+struct ObjetoInt {
+    int peso;
     double valor;
 };
 
@@ -17,7 +24,23 @@ bool operator>(Densidad const &d1, Densidad const &d2) {
     return d1.densidad > d2.densidad;
 }
 
-void mochilaVoraz(std::vector<Objeto> const &objetos, double pesoMochila,
+//Algoritmos -------------------------------------------------------------------
+
+/**
+ * Resuelve el problema de la mochila con objetos fraccionables mediante un
+ * algoritmo voraz.
+ *
+ * Coste en tiempo: O(n logn), n = numero de objetos
+ * Coste en espacio: O(n)
+ *
+ * @todo CAMBIAR COSTE EN ESPACIO A O(1) REDEFINIENDO < DE OBJETOREAL!!!!!!!!!!!
+ *
+ * @param objetos Conjunto de objetos que tenemos disponibles.
+ * @param pesoMochila Peso maximo que soporta la mochila.
+ * @param solucion Indica cuanto se debe coger de cada objeto [0, 1].
+ * @param valorSol Valor de la mochila con los objetos dados por solucion.
+ */
+void mochilaVoraz(std::vector<ObjetoReal> const &objetos, double pesoMochila,
                   std::vector<double> &solucion, double &valorSol) {
     size_t n = objetos.size();
 
@@ -46,10 +69,59 @@ void mochilaVoraz(std::vector<Objeto> const &objetos, double pesoMochila,
     }
 }
 
+/**
+ * Resuelve el problema de la mochila 0-1 mediante un algoritmo de
+ * programacion dinamica. El peso de cada objeto y el peso maximo de la
+ * mochila deben ser enteros positivos.
+ *
+ * Coste: O(nM) en tiempo y espacio, n = numero de objetos, M = pesoMochila
+ *
+ * @param objetos Conjunto de objetos que tenemos disponibles.
+ * @param pesoMochila Peso maximo que soporta la mochila.
+ * @param solucion Indica si se coge el objeto o no.
+ * @param valorSol Valor de la mochila con los objetos dados por solucion.
+ */
+void mochilaProgDin(std::vector<ObjetoInt> const &objetos, int pesoMochila,
+                    std::vector<bool> &solucion, double &valorSol) {
+    size_t n = objetos.size();
+
+    //Creamos e inicializamos a 0 la tabla con la que resolvemos el problema
+    std::vector<std::vector<double>> mochila(n + 1, std::vector<double>
+            (pesoMochila + 1, 0));
+
+    //Rellenamos la tabla
+    //objetos[i - 1] en lugar de i ya que mochila va de [1..n] y objetos va de
+    // [0, n)
+    for (size_t i = 1; i <= n; ++i) {
+        for (int j = 1; j <= pesoMochila; ++j) {
+            if (objetos[i - 1].peso > j)    //Si no cabe no lo cogemos
+                mochila[i][j] = mochila[i - 1][j];
+            else    //Si cabe tomamos el maximo entre cogerlo y no cogerlo
+                mochila[i][j] = std::max(mochila[i - 1][j],
+                                         mochila[i - 1][j -
+                                                        objetos[i - 1].peso] +
+                                         objetos[i - 1].valor);
+        }
+    }
+    valorSol = mochila[n][pesoMochila];
+
+    //Calculamos que objetos hemos cogido
+    int resto = pesoMochila;
+    for(size_t i = n; i >= 1; --i) {
+        if(mochila[i][resto] == mochila[i-1][resto])    //No cogido el objeto i
+            solucion[i-1] = false;
+        else {    //Cogido el objeto i
+            solucion[i-1] = true;
+            resto -= objetos[i-1].peso;
+        }
+    }
+}
+
+//Main -------------------------------------------------------------------------
 
 int main() {
     size_t n = 5;
-    std::vector<Objeto> objetos(n);
+    std::vector<ObjetoInt> objetos(n);
     for (int i = 0; i < n; ++i)
         objetos[i].peso = (i + 1) * 10;
 
@@ -59,12 +131,12 @@ int main() {
     objetos[3].valor = 40;
     objetos[4].valor = 60;
 
-    std::vector<double> solucion(n);
+    std::vector<bool> solucion(n);
     double valorSol;
 
     auto t1 = std::chrono::steady_clock::now();
 
-    mochilaVoraz(objetos, 100, solucion, valorSol);
+    mochilaProgDin(objetos, 100, solucion, valorSol);
 
     auto t2 = std::chrono::steady_clock::now();
     auto time_span = std::chrono::duration_cast<std::chrono::duration<double>>(
